62fb84208d2525f2ad6d6fca4e17c4d1cbd158a9
stephaniaguzman <stephania.guzman@pucp.edu.pe>
2022-04-05 22:06:08 -0500

    distributors lista

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/DistributorsController.java
@@ -4,6 +4,8 @@ package pe.edu.pucp.gtics.lab1221.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pe.edu.pucp.gtics.lab1221.entity.Distributors;
@@ -21,41 +23,44 @@ public class DistributorsController {
     @Autowired
     DistributorsRepository distributorsRepository;
 
-    public String listaDistribuidoras(Model model) {
-        List<Distributors> distribuidorasList = distributorsRepository.findAll();
-        model.addAttribute("distribuidorasList", distribuidorasList);
+    @GetMapping("/lista")
+    public String listaDistribuidoras (Model model){
+        List<Distributors> listaDistribuidoras= distributorsRepository.findAll();
+        model.addAttribute("listaDistribuidoras",listaDistribuidoras);
         return "distribuidoras/lista";
     }
 
+    @GetMapping("/editar")
+    public String editarDistribuidoras(@RequestParam("id") int id, Model model){
+        Optional<Distributors> optionalDistribuidoras = distributorsRepository.findById(id);
+        if (optionalDistribuidoras.isPresent()) {
+            Distributors distribuidoras = optionalDistribuidoras.get();
+            model.addAttribute("distribuidoras", distribuidoras);
+            return "distribuidoras/editar";
+        } else {
+            return "redirect:/distribuidoras/lista";
+        }
+    }
 
-
-    public String editarDistribuidoras(@RequestParam("id")int id, Model model){
-            Optional<Distributors> optionalDistribuidoras = distributorsRepository.findById(id);
-            if(optionalDistribuidoras.isPresent()){
-                Distributors distribuidoras = optionalDistribuidoras.get();
-                model.addAttribute("distribuidoras",distribuidoras);
-                return "distribuidoras/editar";
-            }else {
-                return "redirect:/distribuidoras/lista";
-            }
-    };
-
+    @GetMapping("/nuevo")
     public String nuevaDistribuidora(){
-        return "distribuidoras/lista";
-    };
+        return "distribuidoras/nuevo";
+    }
 
-    public String guardarDistribuidora(Distributors distributors){
-        distributorsRepository.save(distributors);
-            return "redirect:/distribuidoras/lista";
-    };
+    @PostMapping("/guardar")
+    public String guardarDistribuidora(Distributors distribuidoras){
+        distributorsRepository.save(distribuidoras);
+        return "redirect:/distribuidoras/lista";
+    }
 
+    @GetMapping("/borrar")
     public String borrarDistribuidora(@RequestParam("id") int id){
-            Optional<Distributors> optionalDistribuidoras = distributorsRepository.findById(id);
-            if(optionalDistribuidoras.isPresent()){
-                distributorsRepository.deleteById(id);
-            }
-            return "redirect:/distribuidoras/lista";
-    };
+        Optional<Distributors> optionalDistribuidoras = distributorsRepository.findById(id);
+        if (optionalDistribuidoras.isPresent()) {
+            distributorsRepository.deleteById(id);
+        }
+        return "redirect:/distribuidoras/lista";
+    }
 
 }
 //prueba
