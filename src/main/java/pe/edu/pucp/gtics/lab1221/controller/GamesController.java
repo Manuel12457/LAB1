6bc4f2f21fb74c3dcea5f625fc19db036fda5f46
Manuel <a20190133@pucp.edu.pe>
2022-04-05 20:10:03 -0500

    Ejercicio 7

--- src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/controller/GamesController.java
@@ -1,9 +1,30 @@
 package pe.edu.pucp.gtics.lab1221.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pe.edu.pucp.gtics.lab1221.entity.Games;
+import pe.edu.pucp.gtics.lab1221.repository.GamesRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/juegos", method = RequestMethod.GET)
 public class GamesController {
 
-    public String listaJuegos (){
-        return "";
+    @Autowired
+    GamesRepository gamesRepository;
+
+    @GetMapping(" ")
+    public String listaJuegos (Model model){
+
+        List<Games> listaJuegos = gamesRepository.findAll(Sort.by("precio"));
+        model.addAttribute("listaJuegos", listaJuegos);
+        return "/juegos/lista";
     };
 
     public String editarJuegos(){
