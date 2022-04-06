c24cc20050a5f4c2441e374d6845d3e755129ca0
Manuel <a20190133@pucp.edu.pe>
2022-04-05 19:49:48 -0500

    Ejercicio 6

--- src/main/java/pe/edu/pucp/gtics/lab1221/entity/Platforms.java
+++ src/main/java/pe/edu/pucp/gtics/lab1221/entity/Platforms.java
@@ -1,5 +1,45 @@
 package pe.edu.pucp.gtics.lab1221.entity;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "palaformas")
 public class Platforms {
-    /** Completar*/
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idplataforma", nullable = false)
+    private int idplataforma;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    public int getIdplataforma() {
+        return idplataforma;
+    }
+
+    public void setIdplataforma(int idplataforma) {
+        this.idplataforma = idplataforma;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
